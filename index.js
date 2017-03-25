a950869073612d88f66656d105391bd393ca523f
ankihg <hagelin.annika@gmail.com>
2017-03-25 11:03:03 -0700

    btn stylying

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 Vue.component('box', {
-  template: '<h1><slot></slot></h1>'
+  template: '<h1 class="box"><slot></slot></h1>'
+})
+
+Vue.component('btn', {
+  template: '<button><h2><slot></slot></h2></button>>'
 })
 
 var app = new Vue({
